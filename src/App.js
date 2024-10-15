265551933353513b12aaf39dfaa0c9a6a0f35317
Siphe23 <siphesihlemzamane@gmail.com>
2024-10-15 15:19:48 +0200

    Edited README FILE

--- src/App.js
+++ src/App.js
@@ -3,22 +3,16 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
 import Signup from './components/Signup';
-<<<<<<< HEAD
 import Navigation from './components/Navigation';
 import { AuthProvider } from './components/AuthProvider';
 import ProtectedRoute from './components/ProtectedRoute';
-=======
-import Home from './components/Home';
-import Navigation from './components/Navigation'; 
 import './index.css';
->>>>>>> f4e5be2dfa04a2ef57b06682eaa7116d566f2094
 
 function App() {
   return (
-<<<<<<< HEAD
     <AuthProvider>
       <Router>
-        <div>
+        <div className="App">
           <Navigation />
           <div className="container">
             <Routes>
@@ -30,23 +24,7 @@ function App() {
         </div>
       </Router>
     </AuthProvider>
-=======
-    <Router>
-      <div className="App">
-        <Navigation /> 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-      </div>
-    </Router>
->>>>>>> f4e5be2dfa04a2ef57b06682eaa7116d566f2094
   );
 }
 
 export default App;
-
-
-
-
